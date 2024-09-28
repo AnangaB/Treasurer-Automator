9c255ec83e7d52a31e47b596cfc005014e620408
AnangaB <Anangabajgai@gmail.com>
2024-09-28 02:19:20 -0700

    fix: fixed error of incorrect value being sent to form

--- fill_up_cheque_req.py
+++ fill_up_cheque_req.py
@@ -14,7 +14,8 @@ common_info["date_today"]=  datetime.now(pytz.timezone('US/Pacific')).strftime('
 
 #get csv of reimbursemets
 requests = pd.read_csv(r"cheque req requests/cheque_req_requests.csv")
-requests = requests.reset_index(drop=True)
+print(requests)
+print(requests.columns)
 
 # Create a PDF writer to combine forms
 pdf_writer = PdfWriter()
@@ -26,19 +27,21 @@ def fill_out_form(payee_details):
 
     form_data = {"Today s Date":common_info["date_today"],
                 'Group Name' : common_info["group_name"],
-                'Cheque Payable To print legibly': common_info["requested_by"],
+                'Cheque Payable To print legibly': payee_details["Requester"],
                 'In The Amount Of':(payee_details["Amount"]),
                 'Describe the request andor provide additional information if necessary': payee_details["Reason"], 
                 'Requested By': common_info["requested_by"], 
                 'Position':common_info["position"],
-                'Picked up by':"",
-                'Street Address':payee_details["Street Address"],
+                'Picked up by':payee_details["Cheque Picked Up By"],
+                'Email':payee_details["Email"]
+                }
+    """
+       'Street Address':payee_details["Street Address"],
                 'Email':'Email',
                 'City, Province':payee_details["City And Province"],
-                'Postal Code': payee_details["Postal Code"]
-                }
+                'Postal Code': payee_details["Postal Code"]"""
                                                                                                                                                                       #Fill the PDF form into a temporary output path
-    temp_output_path = f"temp_{payee_details.name}.pdf"
+    temp_output_path = f"output forms/temp_{payee_details.name}.pdf"
     fillpdfs.write_fillable_pdf(raw_form, temp_output_path, form_data, flatten=False)
 
     # Add the filled PDF to the writer
@@ -61,6 +64,9 @@ combined_output_path = "output forms/combined_forms.pdf"
 with open(combined_output_path, 'wb') as out_pdf:
     pdf_writer.write(out_pdf)
     
+# remove the temporary files
+for payee_details in requests.itertuples(index=True):
+    os.remove(f"output forms/temp_{payee_details.Index}.pdf")  # Remove temp files after combining
 
 #overwrite requests
-requests.to_csv(r"cheque req requests/cheque_req_requests.csv")
+requests.to_csv(r"cheque req requests/cheque_req_requests.csv", index=False)
