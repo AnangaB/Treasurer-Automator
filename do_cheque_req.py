28338ede724a04c193a3070c4c52bd9f2a32c727
AnangaB <Anangabajgai@gmail.com>
2024-10-05 23:42:52 -0700

    feat: output pdf containing all the filled  reimbursement forms also now each forms corresponding meeting minutes, and blank pages are inserted in between pages of the output pdf to handle issues regarding double sided printing.

--- do_cheque_req.py
+++ do_cheque_req.py
@@ -13,9 +13,18 @@ common_data_file_path = r'./Reimbursement Data/common_info.json'
 """ Returns Requests df and Common Info df, after receiving files from terminal and accessing stored files. Program will exit, if improper data is received. 
     The requests data is a dataset with information to fill reimbursement forms with. 
     The common_info consists of data, that all reimbursement forms will contain.
+
+    The args passed onto the program are:
+        - reimburesement_requests_file_path = sys.argv[1]
+        - datasets containing data of full names and emails of exec = sys.argv[2]
+        - past meeting minutes directory = sys.argv[3]
+        - output dir for forms = sys.argv[4]
+
+
+
 """
-def get_requests_and_common_data():
-    if len(sys.argv) != 4:
+def get_data_via_args():
+    if len(sys.argv) != 5:
         print("Error: Invalid Input Passed in terminal. A command should be of the form:\n python fill_up_cheque_req.py dir/input_file.csv output_file_dir")
         sys.exit(1)
 
@@ -40,12 +49,15 @@ def get_requests_and_common_data():
     
     execs_data_file_path =  sys.argv[2]
     execs_data = pd.read_csv(execs_data_file_path)
+    
+    meeting_mins_dir = sys.argv[3]
+    output_dir = sys.argv[4]
 
-    return common_info, execs_data, requests
+    return common_info, execs_data, requests, meeting_mins_dir, output_dir
 
     """ Creates and saves a pdf, that contains all filled in cheque requestions for funds being taken from core.
     """
-def complete_all_core_requests(core_requests, common_info, execs_data):
+def complete_all_core_requests(core_requests, common_info, execs_data, meeting_mins_folder, output_folder):
     
     #will contain all forms for core_requests reimbursements
     all_core_filled_forms = PdfWriter()
@@ -59,21 +71,33 @@ def complete_all_core_requests(core_requests, common_info, execs_data):
 
         filled_form = get_single_filled_req_form(common_info, payee_details, exec_email)
         all_core_filled_forms.add_page(filled_form)
+        #add blank page after
+        all_core_filled_forms.add_blank_page()
+        #append relevant meeting minutes pdf as well
+        mins_file_path = os.path.join(meeting_mins_folder, payee_details["Minutes File"])
+        all_core_filled_forms.append(mins_file_path)
+        #add blank page, if 
+        if len(all_core_filled_forms.pages) % 2 != 0:
+            all_core_filled_forms.add_blank_page()
+
+
     core_requests.apply(fill_form, axis=1)
 
-    combined_output_path = sys.argv[3]
     output_file_name = str(common_info["date_today"]) + " " + str(len(core_requests)) + " core reimbursements" + ".pdf"
     # Write all filled forms into a single PDF
-    with open(os.path.join(combined_output_path,output_file_name), 'wb') as out_pdf:
+    with open(os.path.join(output_folder,output_file_name), 'wb') as out_pdf:
         all_core_filled_forms.write(out_pdf)
 
 def main():
 
-    common_info,execs_data, requests = get_requests_and_common_data()
+    common_info,execs_data, requests, meeting_mins_dir, output_dir = get_data_via_args()
 
     core_requests = requests.loc[(requests["Fund Type"] == "Core") & ((requests["Form Created"]) == False) ]
     if(len(core_requests) > 0):
-        complete_all_core_requests(core_requests, common_info, execs_data )
+        complete_all_core_requests(core_requests, common_info, execs_data, meeting_mins_dir, output_dir )
+
+    #grant_requests = requests.loc[(requests["Fund Type"] == "Grant") & ((requests["Form Created"]) == False) ]
+
         
 if __name__ == '__main__':
     main()
